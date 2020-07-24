eca25a87a55ff50f133cc29923ccbf042c4aee2d
unknown <CottonNie@boyaa.com>
2020-07-24 17:58:40 +0800

    最小路径和c++实现

--- c++/LeetCode/LeetCode/MinPathSum.cpp
+++ c++/LeetCode/LeetCode/MinPathSum.cpp
@@ -18,17 +18,36 @@
 */
 
 /*
-    解法2：动态规划（可以和A星算法比较）
+    解法2：动态规划（可以和A星算法比较），数组dp[i, j]代表到达某点(i, j)时的最小和，则答案为dp[m, n]，状态转移方程为：dp{i, j} = min(dp{i - 1, j}, dp{i, j - 1})
     缺点：
     知识点：
         1.
 */
 int minPathSum(vector<vector<int>>& grid) {
-    int rslt = 0;
+    int row = grid.size();
+    int column = grid[0].size();
+
+    vector<vector<int>> dp(row, vector<int>(column, 0));
     
+    dp[0][0] = grid[0][0];
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+            if (i - 1 < 0) {
+                dp[i][j] = dp[i][j - 1] + grid[i][j];
+            }
+            else if (j - 1 < 0) {
+                dp[i][j] = dp[i - 1][j] + grid[i][j];
+            }
+            else {
+                dp[i][j] = dp[i - 1][j] <= dp[i][j - 1] ? (dp[i - 1][j] + grid[i][j]) : (dp[i][j - 1] + grid[i][j]);
+            }
+        }
+    }
 
-
-    return rslt;
+    return dp[row - 1][column - 1];
 }
 
 /*
